33cbd2c2b75f8cce687be7b3990ed64bd24c6bc1
AnhDuc2k <khungkhip@gmail.com>
2021-10-02 10:07:22 +0700

    Show and remove Wkrd from DEN. ​​to-do li

--- src/components/ContentPane/DonePane.js
+++ src/components/ContentPane/DonePane.js
@@ -1,9 +1,29 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { Button, Divider, Header, Icon, Segment } from 'semantic-ui-react'
-
+import { refreshWorkDateDataId } from '../../redux/workdates/workDateActions'
+import firebase from '../../firebase'
 class DonePane extends Component {
+  state = {
+    worksRef: firebase.database().ref('works')
+  }
+  handleDeleteWork = work => {
+    const { worksRef } = this.state
+    const { workDateId } = this.props
+
+    worksRef
+      .child(workDateId)
+      .child(work.id)
+      .remove()
+      .then(() => {
+        this.props.refreshWorkDateDataId(Math.random())
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
   render() {
+    const { doneWorks, workDateId } = this.props
     return (
       <Segment stacked>
         <Header>
@@ -11,18 +31,27 @@ class DonePane extends Component {
           <Header.Content>Done</Header.Content>
         </Header>
         <Divider></Divider>
-        <Segment attached clearing>
-          Ten cong viec
-          <Button icon="trash alternate" inverted color="red" floated="right" size="tiny"></Button>
-          <Button icon="checkmark" inverted color="blue" floated="right" size="tiny"></Button>
-        </Segment>
+        {doneWorks &&
+          doneWorks.length > 0 &&
+          doneWorks.map(item => (
+            <Segment attached clearing key={item.id}>
+              {item.name}
+              <Button
+                icon="trash alternate"
+                inverted
+                color="red"
+                floated="right"
+                size="tiny"
+                onClick={() => this.handleDeleteWork(item)}
+              ></Button>
+            </Segment>
+          ))}
       </Segment>
     )
   }
 }
+const mapDispatchToProps = dispatch => ({
+  refreshWorkDateDataId: id => dispatch(refreshWorkDateDataId(id))
+})
 
-const mapStateToProps = state => ({})
-
-const mapDispatchToProps = {}
-
-export default connect(mapStateToProps, mapDispatchToProps)(DonePane)
+export default connect(null, mapDispatchToProps)(DonePane)
